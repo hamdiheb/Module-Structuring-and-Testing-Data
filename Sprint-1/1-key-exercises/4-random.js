b707e053911cb5b0548e8771fd5fea81cd3a5e89
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2025-11-27 11:44:59 +0100

    Done exerice 4-random.js 1-key-exercices from sprint 1

--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -1,9 +1,15 @@
 const minimum = 1;
 const maximum = 100;
 
-const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+let random_number=Math.random();
+const num = Math.floor(random_number * (maximum - minimum + 1)) + minimum;
+
+console.log(`Our random number is ${random_number} and if we multiply it by ${maximum+minimum+1} it will be = ${random_number * (maximum - minimum +1) + minimum} and the closest down round is ${num}`);
 
 // In this exercise, you will need to work out what num represents?
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
+
+
+//Num represent integer number round between 1 and 100
