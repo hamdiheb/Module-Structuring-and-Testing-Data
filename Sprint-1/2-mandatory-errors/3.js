ab35d4279e0250d7ecb3ee0f5ee8b62a173408a3
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2025-11-30 12:53:13 +0100

    Exercice chrome.md from 4-stretch-explore on sprint1

--- Sprint-1/2-mandatory-errors/3.js
+++ Sprint-1/2-mandatory-errors/3.js
@@ -1,7 +1,6 @@
 const cardNumber = '4533787178994213';
 const last4Digits = cardNumber.slice(-4);
 
-
 // The last4Digits variable should store the last 4 digits of cardNumber
 // However, the code isn't working
 // Before running the code, make and explain a prediction about why the code won't work
