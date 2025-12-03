669efae2ebcacfd8a7bdee3bf6b009c4ca03b6c4
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2025-12-03 12:18:30 +0100

    Done 0.js sprint2 1keyerrors

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,15 +1,19 @@
 // Predict and explain first...
-//  =============> This function should return first letter of the input string capitalised + second letter of the input string
+//  =============> This function should return first letter of the input string capitalised + from second letter of the input string till the end using slice function
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
+// ==============>
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  //let str_final = `${str[0].toUpperCase()}${str.slice(1)}`;
+  //return str_final;
+
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
 
-// =============> write your explanation here
-// =============> write your new code here
+// =============> str variable is input that has been declared in the function parameter, we can not redeclare a parameter variable 
+// =============> we can declare new variable to receive the final result or not declare again the variable and return directly
 
 console.log(capitalise("iheb"));
