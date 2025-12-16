2f020c0a2f2a1eec60edf5b27c1e083c44b52f54
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2025-12-16 18:30:05 +0100

    get card value exercice 1keyimplement sprint 3

--- Sprint-3/1-key-implement/3-get-card-value.js
+++ Sprint-3/1-key-implement/3-get-card-value.js
@@ -8,7 +8,16 @@
 // write one test at a time, and make it pass, build your solution up methodically
 // just make one change at a time -- don't rush -- programmers are deep and careful thinkers
 function getCardValue(card) {
-    if (rank === "A") return 11;
+    if (card[0] === "A"){
+      return 11;
+    }else if (Number(card[0]) > 1 && Number(card[0]) < 9 ){
+      return Number(card[0]);
+    }else if (card[0] === "J" || card[0] === "Q" || card[0] === "K" || Number(card) === 10) {
+      return 10;
+    }else {
+      return false;
+    }
+    
 }
 
 // You need to write assertions for your function to check it works in different cases
@@ -33,19 +42,26 @@ assertEquals(aceofSpades, 11);
 // When the function is called with such a card,
 // Then it should return the numeric value corresponding to the rank (e.g., "5" should return 5).
 const fiveofHearts = getCardValue("5♥");
+assertEquals(fiveofHearts, 5);
 // ====> write your test here, and then add a line to pass the test in the function above
 
 // Handle Face Cards (J, Q, K):
 // Given a card with a rank of "10," "J," "Q," or "K",
 // When the function is called with such a card,
+const tencard = getCardValue("10");
+assertEquals(tencard, 10);
 // Then it should return the value 10, as these cards are worth 10 points each in blackjack.
 
 // Handle Ace (A):
 // Given a card with a rank of "A",
 // When the function is called with an Ace,
+const ace = getCardValue("A");
+assertEquals(ace, 11);
 // Then it should, by default, assume the Ace is worth 11 points, which is a common rule in blackjack.
 
 // Handle Invalid Cards:
 // Given a card with an invalid rank (neither a number nor a recognized face card),
 // When the function is called with such a card,
+const invalid_c = getCardValue("I");
+assertEquals(invalid_c, "Invalid card rank.");
 // Then it should throw an error indicating "Invalid card rank."
