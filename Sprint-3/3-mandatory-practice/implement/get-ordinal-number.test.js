c7c839b1367e5ac0a90e4831f33d4693e82600b1
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2025-12-22 15:19:56 +0100

    Added more tests to get ordinal number exercice

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -16,3 +16,23 @@ test("should return '1st' for 1", () => {
 test("should return '25th' for 25", () => {
     expect(getOrdinalNumber(25)).toEqual("25th");
 })
+
+test("Should return '31st' for 31", () => {
+    expect(getOrdinalNumber(31)).toEqual("31st");
+})
+
+test("Expect '20th' for 20", () => {
+    expect(getOrdinalNumber(20)).toEqual("20th");
+})
+
+test("Expect '10th' for 10", () => {
+    expect(getOrdinalNumber(10)).toEqual("10th");
+})
+
+test("Expect '91st' to 91", () => {
+    expect(getOrdinalNumber(91)).toEqual("91st");
+})
+
+test("expect '11th' to 11", () => {
+    expect(getOrdinalNumber(11)).toEqual("11th");
+})
